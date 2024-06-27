2feae0357b218bf6011971f799080c3c82f472be
Athena <bqh1402003@gmail.com>
2024-06-27 09:39:18 +0700

    feat: add LightPistol bullet shot

--- Assets/Scripts/Config/Bullet/BulletConfig.cs
+++ Assets/Scripts/Config/Bullet/BulletConfig.cs
@@ -5,21 +5,18 @@ using UnityEngine;
 
 public class BulletConfig : ScriptableObject
 {
-        public string bulletName;
-        public int damage;
-        public float speed;
-        public int bulletTimeToLive;
-        public float bulletBaseCR;
-        public float bulletBaseCD;
-        public bool destroyOnContact;
-        public int bulletMultiplier;
-        public GameObject bulletPrefab;
+    public string bulletName;
+    public int damage;
+    public float speed;
+    public int bulletTimeToLive;
+    public float bulletBaseCR;
+    public float bulletBaseCD;
+    public bool destroyOnContact;
+    public int bulletMultiplier;
+    public GameObject bulletPrefab;
     public virtual void Fire(Player player)
     {
 
     }
 
 }
-
-
-
