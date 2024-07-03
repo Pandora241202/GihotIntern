4a6d832b48d987bf41182581814cc6009a08988c
Quoc1610 <tranquoc58668@gmail.com>
2024-07-03 10:50:30 +0700

    Add charAsset
    Done anim
    Done Rotate Sync
    Done Char Skin

--- Assets/Scripts/UI/UIMainMenu.cs
+++ Assets/Scripts/UI/UIMainMenu.cs
@@ -29,7 +29,6 @@ public class UIMainMenu : MonoBehaviour
         lsGOPlayer[1].SetActive(false);
         btnStart.onClick.AddListener(() =>
         {
-            UIManager._instance._joystick.gameObject.SetActive(true);
             SendData<EventName> ev = new SendData<EventName>(new EventName("start"));
             SocketCommunication.GetInstance().Send(JsonUtility.ToJson(ev));
         });
