b60d4fedcaba58e2d126e092a1d70ca759701c9f
Quoc1610 <tranquoc58668@gmail.com>
2024-07-04 15:08:08 +0700

    Add map
    -Finish Map
    -Add collsion check
    -Add transparent when between cam

--- Assets/Scripts/GamePlay/CharacterControl.cs
+++ Assets/Scripts/GamePlay/CharacterControl.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class CharacterControl : MonoBehaviour
@@ -9,7 +10,7 @@ public class CharacterControl : MonoBehaviour
 
     [SerializeField] private GameObject prefabBullet;
     public GameObject goChar;
-    [SerializeField] public int gunId; 
+    [SerializeField] public int gunId;
     [SerializeField] LayerMask creepLayerMask;
     private FixedJoystick joystick;
     GameObject curCreepTarget = null;
@@ -20,6 +21,7 @@ public class CharacterControl : MonoBehaviour
     [SerializeField] private Animator charAnim;
     Vector3 colllide_velocity = new Vector3(0, 0, 0);
     bool collision = false;
+
     public CharacterController characterController;
     //public static CharacterControl _instance { get; private set; }
     //public static CharacterControl Instance()
@@ -36,11 +38,11 @@ public class CharacterControl : MonoBehaviour
         //  SocketCommunication.GetInstance();
         //gunId = GameObject.FindAnyObjectByType<SceneUpdater>().bulletManager.GetGunId();
     }
-    private void Start(){
-        
+
+    private void Start()
+    {
         joystick = UIManager._instance._joystick;
     }
-
     public void SetGunAndBullet()
     {
         GunType gunType = AllManager.Instance().bulletManager.gunConfig.lsGunType[gunId];
@@ -52,36 +54,52 @@ public class CharacterControl : MonoBehaviour
 
     private void Update()
     {
-        characterController.Move(velocity);
+        Debug.Log("collide with: " + collision_plane_normal_dict.Count + " obj");
+        //characterController.Move(velocity);
+        if (!collision) transform.position += velocity;
+        else
+        {
+            Vector3 normal = Vector3.zero;
+            for (int i = 0; i < collision_plane_normal_dict.Count; i++)
+            {
+                normal += collision_plane_normal_dict.ElementAt(i).Value;
+            }
+
+            Debug.Log("normal: " + normal);
+            transform.position += (normal + velocity.normalized) * speed * Time.deltaTime;
+        }
+
         Shoot();
         if (velocity != Vector3.zero)
         {
             charAnim.SetBool("isRun", true);
-            
         }
         else
         {
             charAnim.SetBool("isRun", false);
         }
+
         if (id != Player_ID.MyPlayerID) return;
-    
+
         if (frame % 3 == 0)
         {
             float horizontal = joystick.Horizontal;
             float vertical = joystick.Vertical;
-        
-            Vector3 direction = new Vector3(horizontal, 0, vertical);
+
+            Vector3 direction = (new Vector3(horizontal, 0, vertical)).normalized;
             Vector3 velocity = direction * speed * Time.deltaTime;
-            SendData<MoveEvent> data = new SendData<MoveEvent>(new MoveEvent(velocity, transform.position,Quaternion.LookRotation(direction)));
+            SendData<MoveEvent> data =
+                new SendData<MoveEvent>(new MoveEvent(velocity, transform.position,
+                    Quaternion.LookRotation(direction)));
             SocketCommunication.GetInstance().Send(JsonUtility.ToJson(data));
         }
+
         frame++;
     }
 
 
     private void FixedUpdate()
     {
-
     }
 
 
@@ -112,26 +130,28 @@ public class CharacterControl : MonoBehaviour
     {
         GameObject targetObj = GetTagetObj();
 
-        if (targetObj == null) 
+        if (targetObj == null)
         {
             return;
         }
 
-        if (targetObj != curCreepTarget) 
+        if (targetObj != curCreepTarget)
         {
             CreepManager creepManager = AllManager.Instance().creepManager;
             creepManager.MarkTargetCreepById(targetObj.GetInstanceID());
-            if (curCreepTarget != null) 
+            if (curCreepTarget != null)
             {
                 creepManager.UnmarkTargetCreepById(curCreepTarget.GetInstanceID());
-            } 
+            }
+
             curCreepTarget = targetObj;
         }
 
         Vector3 directionToTarget = (targetObj.transform.position - gunTransform.position).normalized;
         Quaternion lookRotation = Quaternion.LookRotation(directionToTarget);
         gunTransform.rotation = lookRotation;
-        lastFireTime = AllManager.Instance().bulletManager.SpawnBullet(gunTransform.position, curCreepTarget, gunId, lastFireTime, "PlayerBullet");
+        lastFireTime = AllManager.Instance().bulletManager
+            .SpawnBullet(gunTransform.position, curCreepTarget, gunId, lastFireTime, "PlayerBullet");
 
         //float angle = Vector3.Angle(gunTransform.forward, directionToTarget);
         //if (angle < 10f)
@@ -139,5 +159,30 @@ public class CharacterControl : MonoBehaviour
         //    AllManager.Instance().bulletManager.SpawnBullet(gunTransform.position, curCreepTarget, gunId);
         //}
     }
+    
+    Dictionary<int, Vector3> collision_plane_normal_dict = new Dictionary<int, Vector3>();
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("MapElement"))
+        {
+            int id = other.gameObject.GetInstanceID();
+            if (collision_plane_normal_dict.ContainsKey(id)) return;
+            Vector3 collide_point = other.ClosestPoint(transform.position);
+            collide_point.y = transform.position.y;
+            collision = true;
+            collision_plane_normal_dict.Add(id, (transform.position - collide_point).normalized);
+        }
+    }
 
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("MapElement"))
+        {
+            if (collision_plane_normal_dict.ContainsKey(other.gameObject.GetInstanceID()))
+            {
+                collision_plane_normal_dict.Remove(other.gameObject.GetInstanceID());
+                if (collision_plane_normal_dict.Count == 0) collision = false;
+            }
+        }
+    }
 }
