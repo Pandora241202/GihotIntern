7bd68ab822886134d5a9267e1ffe610e6bbdb93c
Quoc1610 <tranquoc58668@gmail.com>
2024-07-19 09:38:29 +0700

    Add Asset icon for event

--- Assets/Scripts/UI/UIItem/ItemEvent.cs
+++ Assets/Scripts/UI/UIItem/ItemEvent.cs
@@ -5,9 +5,12 @@ using UnityEngine.UI;
 public class ItemEvent : MonoBehaviour
 {
    [SerializeField] private Image imgProgress;
+   [SerializeField] private Image imgIcon;
+   [SerializeField] private List<Sprite> lsSprite = new List<Sprite>();
    public float duration;
-   public void OnSetUp(int durationEv)
+   public void OnSetUp(int idEvent,int durationEv)
    {
+      imgIcon.sprite = lsSprite[idEvent];
       imgProgress.fillAmount = 0;
       duration = durationEv;
    }
