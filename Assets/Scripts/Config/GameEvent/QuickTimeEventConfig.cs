e2794253d568f8f8cde33a4862fad378bd6d1c13
Quoc1610 <tranquoc58668@gmail.com>
2024-07-19 14:08:06 +0700

    Add Ui for QTE kill

--- Assets/Scripts/Config/GameEvent/QuickTimeEventConfig.cs
+++ Assets/Scripts/Config/GameEvent/QuickTimeEventConfig.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "QuickTimeEventConfig", menuName = "Config/GameEventConfig/QuickTimeEvent")]
@@ -8,11 +9,15 @@ public class QuickTimeEventConfig : GameEventConfig
 {
     private int score;
     private int goal;
+    private bool isActiveOn = false;
+
     public override void Activate(GameEvent gameEvent, GameEventData eventData)
     {
         base.Activate(gameEvent, eventData);
+        isActiveOn = false;
         score = eventData.quick.currentScore;
         goal = eventData.quick.goal;
+
 
         //render current and target score
     }
@@ -21,6 +26,7 @@ public class QuickTimeEventConfig : GameEventConfig
     public override void Apply(GameEvent gameEvent)
     {
         base.Apply(gameEvent);
+
         //update current and target score
     }
 
@@ -36,6 +42,23 @@ public class QuickTimeEventConfig : GameEventConfig
         base.UpdateState(gameEvent, eventData);
         gameEvent.timeEnd = eventData.timeToEnd;
         score = eventData.quick.currentScore;
+        GameObject item;
+        if (!isActiveOn)
+        {
+            
+            if (UIManager._instance.uiGameplay.lsGOEvent.TryGetValue(gameEvent.id, out item))
+            {
+                item.GetComponent<ItemEvent>().goRemainPoint.SetActive(true);
+                item.GetComponent<ItemEvent>().txtPoint.text = (goal - score).ToString();
+                isActiveOn = true;
+            }
+        }
+        if (UIManager._instance.uiGameplay.lsGOEvent.TryGetValue(gameEvent.id, out item))
+        {
+            UIManager._instance.uiGameplay.lsGOEvent[gameEvent.id].gameObject.GetComponent<ItemEvent>()
+                .UpdateScoreQTE(goal - score);
+        }
+        
         Debug.Log(score);
     }
 }
